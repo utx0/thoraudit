610413903299bdfe163bc75f21e43e520726fd60
utx0_ <utx0@mettadata.com>
2021-11-25 16:54:51 +1100

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-//! bondy is a simple little script to find the bond amount
+//! thoraudit is a simple little script to find the bond amount
 //! at the time the thornode went from standby to active in the current churn cycle
 //!
 //! usage: cargo run thor1h0xd53y8yvfsu5t8p6t4ky52h9dzfz3mvsem3z
 
+use std::collections::HashMap;
 use std::env;
 use std::process::exit;
-use serde::Deserialize;
-use serde_json::json;
 use serde_derive::Deserialize;
 
 const BASE_URL: &str = "https://thornode.thorchain.info/thorchain/node";
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 struct NodeInfo {
     node_address: String,
     status: String,
@@ -25,7 +24,7 @@ struct NodeInfo {
     preflight_status: PreflightStatus,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 struct PreflightStatus {
     status: String,
     reason: String,
@@ -50,9 +49,30 @@ async fn main() -> anyhow::Result<()> {
 
     println!("url: {}", url);
 
-    let responce= reqwest::get(url).await?.json::<NodeInfo>().await?;
+    let head_responce= reqwest::get(url).await?.json::<NodeInfo>().await?;
+    let mut node_infos = HashMap::new();
 
-    println!("{:#?}", responce);
+    node_infos.insert(head_responce.status_since, head_responce.clone());
 
+    println!("{:#?}", head_responce);
+
+    let mut status_since = head_responce.status_since;
+
+    while status_since != 0 {
+        let node_info = getblock(node.to_string(), status_since - 1).await?;
+        node_infos.insert(node_info.status_since, node_info.clone());
+        status_since = node_info.status_since;
+    }
+
+    println!("\n{:#?}",node_infos);
     Ok(())
+}
+
+async fn getblock(node: String, height: u32) -> Result<NodeInfo, anyhow::Error> {
+    let url = format!("{}/{}?height={}", BASE_URL,node,height);
+    println!("url: {}", url);
+
+    let resp = reqwest::get(url).await?.json::<NodeInfo>().await?;
+    println!("{:#?}", resp);
+    Ok(resp)
 }
